cd772358407bbd64fe991fe5db8f9ec556ab7302
maestro194 <dongboy1904@gmail.com>
2023-08-11 18:51:58 +0700

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{js,ts,tsx,css}",
+    "./**/*.{html}",
+
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
