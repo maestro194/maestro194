e5c68184a37d0a4ac41aa15162e32e6faef375fd
maestro194 <dongboy1904@gmail.com>
2025-02-07 11:58:02 +0700

    Fix: import issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter } from 'react-router'
+import { Route } from 'react-router-dom'
+
+import Home from './pages/home/Home'
 
 function App() {
   return <BrowserRouter>
