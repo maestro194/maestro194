f2d61ad2cc5c97877b8edc2f5eb4956ae86c7789
maestro194 <dongboy1904@gmail.com>
2025-02-12 07:53:56 +0700

    Feat: Link update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,10 +9,6 @@ import Link from './components/Link.jsx';
 // icon import
 
 export default function Home() {
-    const [time, setTime] = useState({
-        "month": "Feb",
-        "day": "10th"
-    })
 
   return (
     <div className='w-[100vw] h-[100vh] flex items-center justify-center font-(Nunito)'>
@@ -20,7 +16,7 @@ export default function Home() {
             <img src='maestro194/Bento/assets/icons/OneDark/01d.png' className='w-6 h-6'></img>
         </button>
 
-        <img src={backgroundImage} className='bg-cover w-[100vw] absolute h-full opacity-45 -z-1'/>
+        <img src={backgroundImage} className='bg-cover absolute w-full h-full opacity-45 -z-1'/>
 
         <div className="container w-[145vh] h-[85vh] grid grid-cols-4 grid-rows-5 gap-8 p-5">
             <Clock/>
--- src/pages/home/components/Link.jsx
+++ src/pages/home/components/Link.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
 
+const links = [
+  {
+    name: 'GitHub',
+    url: 'github.com/maestro194'
+  },
+  {
+    name: 'Reddit',
+    url: 'reddit.com'
+  },
+  {
+    name: 'YouTube',
+    url: 'youtube.com'
+  },
+  {
+    name: 'Gmail',
+    url: 'mail.google.com'
+  }
+]
+
 export default function Link() {
   return (
     <div className='linksBlock grid row-start-4 row-span-2 col-start-1 col-span-4' id='linksBlock'>
-      <div className="linksBlock grid row-start-3 row-span-2 col-start-1 col-span-4" id="linksBlock">
-        <div className="linksBlockLeft row-start-1 row-span-2 col-start-1 col-span-2" id="linksBlockLeft">
-            <div>
-              Link  
-            </div>
-        </div>
-        <div className="linksBlockRight row-start-1 row-span-2 col-start-3 col-span-2" id="linksBlockRight"></div>
+      <div className="linksBlockLeft row-start-1 row-span-2 col-start-1 col-span-2" id="linksBlockLeft">
+        <ul className="linksList">
+          {links.map((link, index) => (
+            <li key={index}>
+              <a href={link.url} target="_blank" className='mt-2 text-black px-2 py-3 text-bold text-center w-[3/4] bg-white rounded-md shadow-md hover:bg-gray-200'>
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
+
+      <div className="linksBlockRight row-start-1 row-span-2 col-start-3 col-span-2" id="linksBlockRight"></div>
     </div>
   )
 }
